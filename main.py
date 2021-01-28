0fa3d82db07d1db978ed8118c38f8bff17fa9915
João Vítor <joaovitorsf7@hotmail.com>
2021-01-28 08:04:55 -0300

    clear e melhoria no sistema, cadastro.

--- main.py
+++ main.py
@@ -7,11 +7,20 @@
 # from db.showTables import showTables
 # showTables()
 
-name = str(input("Nome: "))
-email = str(input("E-mail: "))
-
-from db.insertInDB import insertInDB
-insertInDB(name, email)
-
 from db.selectInDB import selectInDB
+print("----JJBANK----")
+print("1-Cadastro")
+print("2-Login")
+condicao1 = int(input(":"))
+
+if (condicao1 == 1):
+    from clear import clear
+    clear()
+    print("----CADASTRO----")
+    name = str(input("Nome: "))
+    email = str(input("E-mail: "))
+
+    from db.insertInDB import insertInDB
+    insertInDB(name, email)
+
 selectInDB()
