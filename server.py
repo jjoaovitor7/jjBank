63b8cc8dd43ab3e161f0ffdea8d9dcd66099c713
João Vítor <joaovitorsf7@hotmail.com>
2021-01-31 17:53:40 -0300

    lógica do banco de dados para servidor.

--- server.py
+++ server.py
@@ -2,6 +2,8 @@ import http.server
 import socketserver
 # import json
 import secrets
+from urllib.parse import urlparse, parse_qs
+import pymysql
 
 HOST = "127.0.0.1"
 PORT = 5000
@@ -9,12 +11,83 @@ PORT = 5000
 usuarios_logados = []
 
 
+def doDepositStrategy(email, depositValue):
+    dbConnection = pymysql.connect(
+        host="localhost", user="root", password="123456789", database="test")
+    cursor = dbConnection.cursor()
+
+    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{email}";"""
+    cursor.execute(selectID)
+    fetch = cursor.fetchall()
+    fetchID = int(str(fetch).replace("((", "").replace(",),)", ""))
+
+    selectSaldo = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetchID};"""
+    cursor.execute(selectSaldo)
+    fetchSaldo = cursor.fetchall()
+    deposit = float(str(fetchSaldo).replace(
+        "((", "").replace(",),)", "")) + float(depositValue)
+
+    insertContaBancaria = f"""UPDATE CONTASBANCARIAS SET SALDO="{deposit}" WHERE ID = {fetchID};"""
+    commit = f"""COMMIT;"""
+
+    cursor.execute(insertContaBancaria)
+    cursor.execute(commit)
+
+    dbConnection.close()
+
+
+def viewBalanceStrategy(email):
+    dbConnection = pymysql.connect(
+        host="localhost", user="root", password="123456789", database="test")
+    cursor = dbConnection.cursor()
+
+    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{email}";"""
+    cursor.execute(selectID)
+    fetch = cursor.fetchall()
+    fetchID = int(str(fetch).replace("((", "").replace(",),)", ""))
+
+    selectContaBancaria = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetchID};"""
+
+    cursor.execute(selectContaBancaria)
+    fetchSelectCB = cursor.fetchall()
+
+    dbConnection.close()
+
+    return "Saldo:", float(str(fetchSelectCB).replace(
+        "((", "").replace(",),)", ""))
+
+
+def doWithdrawStrategy(email, withdrawValue):
+    dbConnection = pymysql.connect(
+        host="localhost", user="root", password="123456789", database="test")
+    cursor = dbConnection.cursor()
+
+    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{email}";"""
+    cursor.execute(selectID)
+    fetch = cursor.fetchall()
+    fetchID = int(str(fetch).replace("((", "").replace(",),)", ""))
+
+    selectSaldo = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetchID};"""
+    cursor.execute(selectSaldo)
+    fetchSaldo = cursor.fetchall()
+    withdraw = float(str(fetchSaldo).replace(
+        "((", "").replace(",),)", "")) - float(withdrawValue)
+
+    insertContaBancaria = f"""UPDATE CONTASBANCARIAS SET SALDO={withdraw} WHERE ID = {fetchID};"""
+    commit = f"""COMMIT;"""
+
+    cursor.execute(insertContaBancaria)
+    cursor.execute(commit)
+
+    dbConnection.close()
+
+
 class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
-    # def do_GET(self):
-    #     self.send_response(200)
-    #     response = json.dumps({"logged_users": usuarios_logados})
-    #     response = bytes(response, 'utf-8')
-    #     self.wfile.write(response)
+    def do_GET(self):
+        if (self.path.startswith("/viewbalance")):
+            query = parse_qs(urlparse(self.path).query)
+            self.send_response(200, viewBalanceStrategy(query['email'][0]))
+            self.end_headers()
 
     def do_POST(self):
         if (self.path.endswith("/")):
@@ -34,6 +107,18 @@ class SimpleHTTPRequestHandler(http.server.BaseHTTPRequestHandler):
             else:
                 self.send_response(404, False)
                 self.end_headers()
+
+        elif (self.path.startswith("/deposit")):
+            query = parse_qs(urlparse(self.path).query)
+            doDepositStrategy(query['email'][0], query['depositValue'][0])
+            self.send_response(200)
+            self.end_headers()
+
+        elif (self.path.startswith("/withdraw")):
+            query = parse_qs(urlparse(self.path).query)
+            doWithdrawStrategy(query['email'][0], query['withdrawValue'][0])
+            self.send_response(200)
+            self.end_headers()
 
     def do_DELETE(self):
         self.data_string = self.rfile.read(
--- src/optionsAccount.py
+++ src/optionsAccount.py
@@ -1,5 +1,4 @@
 import globals
-import pymysql
 from clear import clear
 import requests
 
@@ -10,78 +9,6 @@ def context(strategy):
 
 def viewProfileStrategy():
     print(f"NOME: {globals.NAME}\nE-MAIL: {globals.EMAIL}")
-
-
-def doDepositStrategy():
-    dbConnection = pymysql.connect(
-        host="localhost", user="root", password="123456789", database="test")
-    cursor = dbConnection.cursor()
-
-    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{globals.EMAIL}";"""
-    cursor.execute(selectID)
-    fetch = cursor.fetchall()
-    fetchID = int(str(fetch).replace("((", "").replace(",),)", ""))
-
-    depositValue = float(input("Depósitar (R$): "))
-
-    selectSaldo = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetchID};"""
-    cursor.execute(selectSaldo)
-    fetchSaldo = cursor.fetchall()
-    deposit = float(str(fetchSaldo).replace(
-        "((", "").replace(",),)", "")) + depositValue
-
-    insertContaBancaria = f"""UPDATE CONTASBANCARIAS SET SALDO="{deposit}" WHERE ID = {fetchID};"""
-    commit = f"""COMMIT;"""
-
-    cursor.execute(insertContaBancaria)
-    cursor.execute(commit)
-
-    dbConnection.close()
-
-
-def viewBalanceStrategy():
-    dbConnection = pymysql.connect(
-        host="localhost", user="root", password="123456789", database="test")
-    cursor = dbConnection.cursor()
-
-    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{globals.EMAIL}";"""
-    cursor.execute(selectID)
-    fetch = cursor.fetchall()
-    fetch = int(str(fetch).replace("((", "").replace(",),)", ""))
-
-    selectContaBancaria = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetch};"""
-
-    cursor.execute(selectContaBancaria)
-    print("Saldo:", float(str(cursor.fetchall()).replace(
-        "((", "").replace(",),)", "")))
-    dbConnection.close()
-
-
-def doWithdrawStrategy():
-    dbConnection = pymysql.connect(
-        host="localhost", user="root", password="123456789", database="test")
-    cursor = dbConnection.cursor()
-
-    selectID = f"""SELECT ID FROM USERS WHERE EMAIL = "{globals.EMAIL}";"""
-    cursor.execute(selectID)
-    fetch = cursor.fetchall()
-    fetchID = int(str(fetch).replace("((", "").replace(",),)", ""))
-
-    withdrawValue = float(input("Saque (R$): "))
-
-    selectSaldo = f"""SELECT SALDO FROM CONTASBANCARIAS WHERE ID = {fetchID};"""
-    cursor.execute(selectSaldo)
-    fetchSaldo = cursor.fetchall()
-    withdraw = float(str(fetchSaldo).replace(
-        "((", "").replace(",),)", "")) - withdrawValue
-
-    insertContaBancaria = f"""UPDATE CONTASBANCARIAS SET SALDO={withdraw} WHERE ID = {fetchID};"""
-    commit = f"""COMMIT;"""
-
-    cursor.execute(insertContaBancaria)
-    cursor.execute(commit)
-
-    dbConnection.close()
 
 
 def options():
@@ -104,17 +31,22 @@ def options():
 
             elif(condicao2 == 2):
                 clear()
-                context(doDepositStrategy())
+                depositValue = float(input("Depósitar (R$): "))
+                requests.post("http://127.0.0.1:5000/deposit",
+                              params={"email": globals.EMAIL, "depositValue": depositValue})
                 print("\n")
 
             elif (condicao2 == 3):
                 clear()
-                context(viewBalanceStrategy())
+                print(requests.get("http://127.0.0.1:5000/viewbalance",
+                                   params={"email": globals.EMAIL}).reason)
                 print("\n")
 
             elif (condicao2 == 4):
                 clear()
-                context(doWithdrawStrategy())
+                withdrawValue = float(input("Saque (R$): "))
+                requests.post("http://127.0.0.1:5000/withdraw",
+                             params={"email": globals.EMAIL, "withdrawValue": withdrawValue})
                 print("\n")
 
             elif (condicao2 == 5):
